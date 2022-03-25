c05070d736744227ca03b243cd56361acf0e8d55
sthome <sandro.thome@outlook.com>
2022-03-24 21:35:54 -0300

    Removed all cached files and re-added all after changes made to .gitignore

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    // Has to be a nullable variable because findViewById() can only be called after setContentView() runs
+    // in this case to avoid having to do null checks we initialize the variable in another way.
+    // var diceImage: ImageView? = null
+
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,12 +20,21 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when(Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // It's no good practice call findViewById() everytime this method runs
+        // val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
 
-        resultText.text = randomInt.toString()
     }
 }
